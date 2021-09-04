ec4bf1018cb1b53b7684757987b7ee2bdc28c2b5
Yube <eduardo.yubero.ext@singularbank.es>
2021-09-05 01:33:37 +0200

    initial project status

---
+++ app/src/main/java/com/eyubero/marvelbank/MainActivity.kt
@@ -0,0 +1,16 @@
+package com.eyubero.marvelbank
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.util.Log
+import androidx.navigation.NavController
+import androidx.navigation.fragment.NavHostFragment
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+
+}
